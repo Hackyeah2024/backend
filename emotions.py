9e07e60b25fb7a031fad3189bd51dd291f0b5864
piotr <piotrkrzemien11@gmail.com>
2024-09-29 10:35:09 +0200

    Additional properties

--- emotions.py
+++ emotions.py
@@ -1,38 +1,42 @@
-import cv2, os, math
-from google.cloud import vision
+import cv2
+# import os, math
+# from google.cloud import vision
 
 
 def detect_emotions(video_path):
-    client = vision.ImageAnnotatorClient()
-
     vidcap = cv2.VideoCapture(video_path)
+
+    # client = vision.ImageAnnotatorClient()
     # frame_rate = vidcap.get(cv2.CAP_PROP_FPS)
     # while vidcap.isOpened():
     #     frame_id = vidcap.get(1)  # current frame number
     #     ret, frame = vidcap.read()
     #     if not ret:
     #         break
+    #
+    #     if frame_id % math.floor(frame_rate) == 0:
+    #         print("Processing frame {frame_id}".format(frame_id=frame_id))
+    #         image = vision.Image(content=frame)
+    #         response = client.face_detection(image=image)
+    #         faces = response.face_annotations
+    #         likelihood_name = (
+    #             "UNKNOWN",
+    #             "VERY_UNLIKELY",
+    #             "UNLIKELY",
+    #             "POSSIBLE",
+    #             "LIKELY",
+    #             "VERY_LIKELY",
+    #         )
+    #         for face in faces:
+    #             print(f"anger: {likelihood_name[face.anger_likelihood]}")
+    #             print(f"joy: {likelihood_name[face.joy_likelihood]}")
+    #             print(f"surprise: {likelihood_name[face.surprise_likelihood]}")
+    #
+    #         path = os.path.abspath("frames/frame%d.jpg" % int(frameId))
+    #         cv2.imwrite(path, frame)
 
-        # if frame_id % math.floor(frame_rate) == 0:
-        #     print("Processing frame {frame_id}".format(frame_id=frame_id))
-        #     image = vision.Image(content=frame)
-        #     response = client.face_detection(image=image)
-        #     faces = response.face_annotations
-        #     likelihood_name = (
-        #         "UNKNOWN",
-        #         "VERY_UNLIKELY",
-        #         "UNLIKELY",
-        #         "POSSIBLE",
-        #         "LIKELY",
-        #         "VERY_LIKELY",
-        #     )
-        #     for face in faces:
-        #         print(f"anger: {likelihood_name[face.anger_likelihood]}")
-        #         print(f"joy: {likelihood_name[face.joy_likelihood]}")
-        #         print(f"surprise: {likelihood_name[face.surprise_likelihood]}")
-
-        #     path = os.path.abspath("frames/frame%d.jpg" % int(frameId))
-        #     cv2.imwrite(path, frame)
-    duration = vidcap.get(cv2.CAP_PROP_POS_MSEC)
+    fps = vidcap.get(cv2.CAP_PROP_FPS)
+    frame_count = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
+    duration = frame_count/fps
     vidcap.release()
     return None, duration
